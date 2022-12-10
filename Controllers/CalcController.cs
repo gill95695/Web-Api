f071c164a6b2ccfdd83aca294ad865f7076c3d2d
Amanjot Singh Gill <amanjotgill95695@gmail.com>
2022-12-10 23:05:47 +0530

    Initial commit

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,44 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class CalcController : Controller
+    {
+        //
+        // GET: /Calc/
+
+        public ActionResult Index()
+        {
+            return View(new Calc());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Calc c,string calculate)
+        {
+            if (calculate == "add")
+            {
+                c.tot = c.no1 + c.no2;
+            }
+            else if (calculate == "min")
+            {
+                c.tot = c.no1 - c.no2;
+            }
+            else if (calculate == "mul")
+            {
+                c.tot = c.no1 * c.no2;
+            }
+            else
+            {
+                c.tot = c.no1 / c.no2;
+            }
+
+            return View(c);
+        }
+
+    }
+}
---
+++ Models/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Calc
+    {
+        public int no1 { get; set; }
+
+        public int no2 { get; set; }
+
+        public int tot { get; set; }
+    }
+}
